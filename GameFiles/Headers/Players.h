6dc07779358936294a249c25086bdbf2ea617b71
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-04-27 14:31:41 +0300

    Add files via upload

---
+++ GameFiles/Headers/Players.h
@@ -0,0 +1,30 @@
+#pragma once
+struct Players
+{
+	Font score_Tex;
+	Text score;
+	int Score = 0;
+	Sprite character;
+	Texture texture;
+	float velocity_x;
+	float velocity_y;
+	float x;
+	bool check_on_ground = 1;
+	int curr_colission = 0;
+	const float gravity = 4.0f;
+	const float jumpVelocity = 900.0f;
+	bool pree;
+	bool pree2;
+	int reflectionR;
+	int reflectionL;
+	const float Ground = 900;
+	const float Right_wall = 1500;
+	const float Left_wall = 450;
+	int droptype = -1;
+	float incspeed = 1, addspeed = 0, superjump = 1, addsuperjump = 0;
+	Sound so4;
+	void inti(Texture& texture);
+	void update();
+	void Players_Motion(SoundBuffer& buff, Keyboard::Key left, Keyboard::Key right, Keyboard::Key jump);
+};
+
---
+++ GameFiles/Headers/PowerUps.h
@@ -0,0 +1,21 @@
+#pragma once
+struct PowerUps
+{
+	Sprite dropShape;		   // random powerup
+	//RectangleShape blockShape; // random block
+	int type;				   // drop type
+	int PowerUP_veolcity;
+	Texture DropsTex[4];
+	Sprite Drops[4]; // 0 walls inwards(heart)
+	float velocity_x = 20;
+	bool skip = 0;
+
+	void setDrops();
+	void generateDrop(Vector2f stair_position, bool check);
+	void dropcollision();
+	void checkdrop(Clock& timerOfMove, bool& start, bool& StartReturning);
+	void resetPowerups();
+};
+
+
+

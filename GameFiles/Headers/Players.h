012d1f5eb7e47baa8f39e564b0e6206d5a91eb29
Mostafa Abdo <160092209+MostafaAbdoo@users.noreply.github.com>
2024-04-28 22:21:03 +0300

    Update Players.h

--- GameFiles/Headers/Players.h
+++ GameFiles/Headers/Players.h
@@ -1,30 +1,31 @@
 #pragma once
 struct Players
 {
-	Font score_Tex;
-	Text score;
-	int Score = 0;
-	Sprite character;
-	Texture texture;
-	float velocity_x;
-	float velocity_y;
-	float x;
-	bool check_on_ground = 1;
-	int curr_colission = 0;
-	const float gravity = 0.2f;
-	const float jumpVelocity = 10.f;
-	bool pree;
-	bool pree2;
-	int reflectionR;
-	int reflectionL;
-	const float Ground = 900;
-	const float Right_wall = 1500;
-	const float Left_wall = 450;
-	int droptype = -1;
-	float incspeed = 1, addspeed = 0, superjump = 1, addsuperjump = 0;
-	Sound so4;
-	void inti(Texture& texture);
-	void update();
-	void Players_Motion(SoundBuffer& buff, Keyboard::Key left, Keyboard::Key right, Keyboard::Key jump);
+    Font score_Tex;
+    Text score;
+    int Score = 0;
+    Sprite character;
+    Texture texture;
+    float velocity_x;
+    float velocity_y;
+    float x;
+    bool check_on_ground = 1;
+    int curr_colission = 0;
+    const float gravity = 0.2f;
+    const float jumpVelocity = 10.f;
+    bool pree;
+    bool pree2;
+    int reflectionR;
+    int reflectionL;
+    const float Ground = 900;
+    const float Right_wall = 1500;
+    const float Left_wall = 450;
+    int droptype = -1;
+    float PosCnt = 1.0;
+    float NegCnt = 1.0;
+    float incspeed = 1, addspeed = 0, superjump = 1, addsuperjump = 0;
+    Sound so4;
+    void inti(Texture& texture);
+    void update();
+    void Players_Motion(SoundBuffer& buff, Keyboard::Key left, Keyboard::Key right, Keyboard::Key jump);
 };
-

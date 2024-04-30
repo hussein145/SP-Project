a6b0500f50b47fb4d93616650d87bb51c5de3d3a
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-04-30 17:17:11 +0300

    Update Players.h

--- GameFiles/Headers/Players.h
+++ GameFiles/Headers/Players.h
@@ -9,14 +9,15 @@ struct Players
 	float j = 0.f;
 	float velocity_x = 0;
 	float velocity_y = 0;
-	float Motion_Velocity = 6.f;
+	float jump_height;
+	float Motion_Velocity;
 	bool validL = 0, validR = 0;
 	float x;
 	float t = 0.f;
 	bool check_on_ground = 1;
 	int curr_colission = 0;
-	const float gravity = 0.2f;
-	const float jumpVelocity = 9.f;
+	const float gravity = 17.f;
+	const float jumpVelocity = 8.f;
 	const float Ground = 900;
 	int droptype = -1;
 	float incspeed = 1, addspeed = 0, superjump = 1, addsuperjump = 0;

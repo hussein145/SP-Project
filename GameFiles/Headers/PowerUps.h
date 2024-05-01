dea011ebfc65597087c51807249bcc6da3351687
ahmedessamsh <71143516+ahmedessamsh@users.noreply.github.com>
2024-05-02 00:04:54 +0300

    Add files via upload

--- GameFiles/Headers/PowerUps.h
+++ GameFiles/Headers/PowerUps.h
@@ -1,15 +1,16 @@
 #pragma once
 struct PowerUps
 {
-	PowerUps* dropBag;
 	Sprite dropShape;		   // random powerup
 	//RectangleShape blockShape; // random block
 	int type;				   // drop type
 	int PowerUP_veolcity;
-	Texture DropsTex[4];
-	Sprite Drops[4]; // 0 walls inwards(heart)
+	Texture DropsTex[5];
+	Sprite Drops[5]; // 0 walls inwards(heart)
 	float velocity_x = 20;
 	bool skip = 0;
+	float stopsmall = 0, stopbig = 0;
+	int rando;
 
 	void setDrops();
 	void generateDrop(Vector2f stair_position, bool check);

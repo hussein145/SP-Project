256b0b37dca610181d4e570ea354eacee24b6505
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-05-02 00:24:11 +0300

    Update PowerUps.cpp

--- GameFiles/Source/PowerUps.cpp
+++ GameFiles/Source/PowerUps.cpp
@@ -85,10 +85,13 @@ void PowerUps::checkdrop(Clock& timerOfMove, bool& start, bool& StartReturning) 
 					Stairs.stairs[i].move(-velocity_x * dt, 0);
 				else if (Stairs.stairs[i].getPosition().x < 960)
 					Stairs.stairs[i].move(velocity_x * dt, 0);
-
+			}
+			for (int i = 0; i < background.bgNums; i++)
+			{
 				background.wallsLeft[i].move(velocity_x * dt, 0);
 				background.wallsRight[i].move(-velocity_x * dt, 0);
 			}
+			
 			gameclock.cl.move(velocity_x * dt, 0);
 			gameclock.cl2.move(velocity_x * dt, 0);
 		}

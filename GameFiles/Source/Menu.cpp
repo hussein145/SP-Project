57a2300ec3392fdde4061d27b5dd98aae563e394
ابانوب نصيف برزى مشرقى <153004639+BeboNasif@users.noreply.github.com>
2024-05-02 23:32:52 +0300

    Update Menu.cpp

--- GameFiles/Source/Menu.cpp
+++ GameFiles/Source/Menu.cpp
@@ -8,6 +8,10 @@
 #include "Sounds.h"
 using namespace std;
 using namespace sf;
+int PLayer1 = 0;
+int PLayer2 = 0;
+extern bool END;
+bool pressed = false;
 extern Sounds sound;
 menu_Bg_and_Face menu_UI;
 void Gameplay();
@@ -23,12 +27,12 @@ void Menu::MoveDown(int& selected, int choises)
 	if (selected < choises)
 	{
 		mainmenu[selected].setFillColor(Color::Black);
-		hand.setPosition(1140, hand.getPosition().y + positionOfHand);
+		hand.setPosition(hand.getPosition().x, hand.getPosition().y + positionOfHand);
 		selected++;
 		if (selected == choises)
 		{
 			selected = 0;
-			hand.setPosition(1140, 580);
+			hand.setPosition(hand.getPosition().x, hand.getPosition().y - (choises * positionOfHand));
 		}
 		mainmenu[selected].setFillColor(Color{ 255,204,0 });
 	}
@@ -39,12 +43,12 @@ void Menu::MoveUp(int& selected, int choises)
 	{
 
 		mainmenu[selected].setFillColor(Color::Black);
-		hand.setPosition(1140, hand.getPosition().y - positionOfHand);
+		hand.setPosition(hand.getPosition().x, hand.getPosition().y - positionOfHand);
 		selected--;
 		if (selected == -1)
 		{
 			selected = choises - 1;
-			hand.setPosition(1140, hand.getPosition().y + (positionOfHand * choises));
+			hand.setPosition(hand.getPosition().x, hand.getPosition().y + (positionOfHand * choises));
 		}
 		mainmenu[selected].setFillColor(Color{ 255,204,0 });
 
@@ -55,12 +59,12 @@ void  Menu::menu1(RenderWindow& window, int& GameMode)
 	Menu men1;
 	sound.LoadMusic();
 	sound.music(0);
-	men1.font.loadFromFile("Assets/Fonts/Freedom-10eM.ttf");
+	men1.font.loadFromFile("Assets/Fonts/HalloweenSlimePersonalUse-4B80D.otf");
 	enternameTX.loadFromFile("Assets/Textures/entername.png");
 	enternameSP.setTexture(enternameTX);
 	enternameSP.setPosition(500, 450);
 	enternameSP.setScale(2, 2);
-	playernamefont.loadFromFile("Assets/Fonts/Freedom-10eM.ttf");
+	playernamefont.loadFromFile("Assets/Fonts/HalloweenSlimePersonalUse-4B80D.otf");
 
 	playerNameText.setFont(playernamefont);
 	playerNameText.setCharacterSize(24);
@@ -156,7 +160,7 @@ void  Menu::menu1(RenderWindow& window, int& GameMode)
 
 				}
 				playerNameText.setString(playername);
-				if(infile)
+				if (infile)
 				{
 					if (event.key.code == Keyboard::Escape && !pressed && men1.selected != 5 + resusme)
 					{
@@ -166,15 +170,6 @@ void  Menu::menu1(RenderWindow& window, int& GameMode)
 						men1.mainmenu[5 + resusme].setFillColor(Color{ 255,204,0 });
 						men1.hand.setPosition(1140, 890 + shift);
 						pressed = true;
-					}
-
-					if (event.key.code == Keyboard::Escape && !pressed && men1.selected == 5 + resusme)
-					{
-						window.close();
-					}
-					if (!Keyboard::isKeyPressed(Keyboard::Escape))
-					{
-						pressed = false;
 					}
 					if (event.type == Event::KeyPressed)
 					{
@@ -190,7 +185,7 @@ void  Menu::menu1(RenderWindow& window, int& GameMode)
 							//so.play();
 							men1.MoveUp(men1.selected, men1.choises);
 						}
-						if (event.key.code == Keyboard::Enter)
+						if (event.key.code == Keyboard::Enter || event.key.code == Keyboard::Escape && !pressed && men1.selected == 5 + resusme)
 						{
 							sound.select_option_Sound();
 							if (men1.selected == 5 + resusme)
@@ -204,6 +199,10 @@ void  Menu::menu1(RenderWindow& window, int& GameMode)
 							if (men1.selected == 1 + resusme)
 								instructions(window);
 						}
+					}
+					if (!Keyboard::isKeyPressed(Keyboard::Escape))
+					{
+						pressed = false;
 					}
 
 				}
@@ -234,8 +233,8 @@ void  Menu::Play_menu(RenderWindow& window, int& GameMode)
 {
 	Menu menu2;
 	menu2.Hand_intilization();
-	menu2.font.loadFromFile("Assets/Fonts/Freedom-10eM.ttf");
-	menu2.choises = 3;
+	menu2.font.loadFromFile("Assets/Fonts/HalloweenSlimePersonalUse-4B80D.otf");
+	menu2.choises = 4;
 
 	menu2.mainmenu[0].setFont(menu2.font);
 	menu2.mainmenu[0].setString("Single");
@@ -255,27 +254,44 @@ void  Menu::Play_menu(RenderWindow& window, int& GameMode)
 	menu2.mainmenu[2].setFillColor(Color::Black);
 	menu2.mainmenu[2].setPosition(Vector2f(1250, menu2.height / 2 + 160));
 
+	menu2.mainmenu[3].setFont(menu2.font);
+	menu2.mainmenu[3].setString("Back");
+	menu2.mainmenu[3].setCharacterSize(50);
+	menu2.mainmenu[3].setFillColor(Color::Black);
+	menu2.mainmenu[3].setPosition(Vector2f(1250, menu2.height / 2 + 220));
+
 	pageNumber = 500;
 
 	while (window.isOpen())
 	{
 		Event event;
-
-		if (Keyboard::isKeyPressed(Keyboard::Escape) && !pressed)
-		{
-			sound.select_option_Sound();
-			pageNumber = 1000;
-			pressed = true;
-			break;
-		}
-		if (!Keyboard::isKeyPressed(Keyboard::Escape))
-		{
-			pressed = false;
-		}
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
 				window.close();
+			if (event.key.code == Keyboard::Enter || (event.key.code == Keyboard::Escape && !pressed))
+			{
+				sound.select_option_Sound();
+				if (menu2.selected == 3) {
+					pageNumber = 1000;
+					pressed = true;
+					return;
+				}
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Escape) && menu2.selected != 3 && !pressed)
+			{
+				sound.change_option_Sound();
+				menu2.mainmenu[menu2.selected].setFillColor(Color::Black);
+				menu2.selected = 3;
+				menu2.mainmenu[3].setFillColor(Color{ 255,204,0 });
+				menu2.hand.setPosition(1140, 760);
+				pressed = true;
+			}
+			if (!Keyboard::isKeyPressed(Keyboard::Escape))
+			{
+				pressed = false;
+			}
 			if (event.type == Event::KeyPressed)
 			{
 				if (event.key.code == Keyboard::Down) {
@@ -306,45 +322,45 @@ void  Menu::Play_menu(RenderWindow& window, int& GameMode)
 			window.draw(menu2.mainmenu[i]);
 		}
 		window.draw(menu2.hand);
-		
+
 		window.display();
 	}
 }
 void  Menu::options_menu1(RenderWindow& window)
 {
-	Menu menu4;
-	menu4.Hand_intilization();
-	int charcter = 0;
-	Font font;
-	font.loadFromFile("Assets/Fonts/Freedom-10eM.ttf");
-	Text t1;
+	Menu menu5;
+	menu5.Hand_intilization();
+	menu5.positionOfHand = 110;
+	menu5.font.loadFromFile("Assets/Fonts/HalloweenSlimePersonalUse-4B80D.otf");
+	menu5.choises = 3;
 
-	t1.setFont(font);
-	t1.setString("change charcter <> ");
-	t1.setCharacterSize(70);
-	t1.setPosition(10, 10);
-	t1.setFillColor(Color{ 255,204,0 });
-	t1.setPosition(650, 460);
+	menu5.mainmenu[0].setFont(menu5.font);
+	menu5.mainmenu[0].setFillColor(Color{ 255,204,0 });
+	menu5.mainmenu[0].setString("Player1 \n Charchters <>");
+	menu5.mainmenu[0].setCharacterSize(50);
+	menu5.mainmenu[0].setPosition(Vector2f(1250, menu5.height / 2 + 40));
 
+	menu5.mainmenu[1].setFont(menu5.font);
+	menu5.mainmenu[1].setFillColor(Color::Black);
+	menu5.mainmenu[1].setString("Player2 \n Charchters <>");
+	menu5.mainmenu[1].setCharacterSize(50);
+	menu5.mainmenu[1].setPosition(Vector2f(1250, menu5.height / 2 + 150));
 
-	Texture Bl;
-	Bl.loadFromFile("Assets/Textures/Stair.png");
-
-	Sprite Block;
-	Block.setTexture(Bl);
-
-	Block.setScale(.8, 1);
-	Block.setPosition(1245, 550);
+	menu5.mainmenu[2].setFont(menu5.font);
+	menu5.mainmenu[2].setFillColor(Color::Black);
+	menu5.mainmenu[2].setString("Back");
+	menu5.mainmenu[2].setCharacterSize(50);
+	menu5.mainmenu[2].setPosition(Vector2f(1250, menu5.height / 2 + 260));
 
 	Texture pl1;
-	pl1.loadFromFile("Assets/Textures/Dude_Monster.png");
+	pl1.loadFromFile("Assets/Textures/icytower1.png");
 
 	Texture pl2;
-	pl2.loadFromFile("Assets/Textures/Owlet_Monster.png");
-	Texture pl3;
-	pl3.loadFromFile("Assets/Textures/Pink_Monster.png");
+	pl2.loadFromFile("Assets/Textures/icytower2.png");
 
-	Sprite player;
+	Sprite player1;
+	Sprite player2;
+
 	while (window.isOpen())
 	{
 		Event event;
@@ -352,52 +368,104 @@ void  Menu::options_menu1(RenderWindow& window)
 		{
 			if (event.type == event.Closed)
 				window.close();
-			if (Keyboard::isKeyPressed(Keyboard::Left))
-			{
-				if (charcter > 0)
-					charcter--;
-				else
-					charcter = 2;
+
+			if (Keyboard::isKeyPressed(Keyboard::Down)) {
+				sound.change_option_Sound();
+				menu5.MoveDown(menu5.selected, menu5.choises);
 			}
-			if (Keyboard::isKeyPressed(Keyboard::Right))
-			{
-				if (charcter < 2)
-					charcter++;
-				else
-					charcter = 0;
+			if (Keyboard::isKeyPressed(Keyboard::Up)) {
+				sound.change_option_Sound();
+				menu5.MoveUp(menu5.selected, menu5.choises);
 			}
-			if (Keyboard::isKeyPressed(Keyboard::Escape))
+			if (Keyboard::isKeyPressed(Keyboard::Escape) && menu5.selected != 2&& !pressed)
 			{
-				pageNumber = 1000;
-				return;
+				sound.change_option_Sound();
+				menu5.mainmenu[menu5.selected].setFillColor(Color::Black);
+				menu5.selected = 2;
+				menu5.mainmenu[2].setFillColor(Color{ 255,204,0 });
+				menu5.hand.setPosition(1140, 800);
+				pressed = true;
+			}
+			if (event.key.code == Keyboard::Enter || (event.key.code == Keyboard::Escape&& !pressed))
+			{
+				sound.select_option_Sound();
+				if (menu5.selected == 2) {
+					return;
+				}
+			}
+			if (!Keyboard::isKeyPressed(Keyboard::Escape))
+			{
+				pressed = false;
 			}
 
+			if (menu5.selected == 0)
+			{
+				if (Keyboard::isKeyPressed(Keyboard::Left))
+				{
+					if (PLayer1 > 0)
+						PLayer1--;
+					else
+						PLayer1 = 1;
+				}
+				if (Keyboard::isKeyPressed(Keyboard::Right))
+				{
+					if (PLayer1 < 1)
+						PLayer1++;
+					else
+						PLayer1 = 0;
+				}
+			}
+			else if (menu5.selected == 1)
+			{
+				if (Keyboard::isKeyPressed(Keyboard::Left))
+				{
+					if (PLayer2 > 0)
+						PLayer2--;
+					else
+						PLayer2 = 1;
+				}
+				if (Keyboard::isKeyPressed(Keyboard::Right))
+				{
+					if (PLayer2 < 1)
+						PLayer2++;
+					else
+						PLayer2 = 0;
+				}
+			}
 		}
+		if (PLayer1 == 0)
+		{
+			player1.setTexture(pl1);
+		}
+		else if (PLayer1 == 1)
+		{
+			player1.setTexture(pl2);
+		}
+		if (PLayer2 == 1)
+		{
+			player2.setTexture(pl1);
+		}
+		else if (PLayer2 == 0)
+		{
+			player2.setTexture(pl2);
+		}
+		player1.setTextureRect(IntRect(0, 0, 50, 60));
+		player1.setScale(2.4, 2.4);
+		player1.setPosition(1650, menu5.height / 2);
+		player2.setTextureRect(IntRect(0, 0, 50, 60));
+		player2.setScale(2.4, 2.4);
+		player2.setPosition(1650, menu5.height / 2 + 150);
 		menu_UI.FaceMotion(window);
-		if (charcter == 0)
-		{
-			player.setTexture(pl1);
-			player.setScale(3, 3);
-			player.setPosition(1250, 440);
-		}
-		else if (charcter == 1)
-		{
-			player.setTexture(pl2);
-			player.setScale(3, 3);
-			player.setPosition(1250, 440);
-		}
-		else if (charcter == 2)
-		{
-			player.setTexture(pl3);
-			player.setScale(3, 3);
-			player.setPosition(1250, 440);
-		}
 		window.clear();
 		window.draw(menu_UI.bg);
+		for (int i = 0; i < menu5.choises; i++)
+		{
+			window.draw(menu5.mainmenu[i]);
+		}
 		window.draw(menu_UI.face);
-		window.draw(t1);
-		window.draw(Block);
-		window.draw(player);
+		window.draw(menu5.hand);
+		window.draw(player1);
+		window.draw(player2);
 		window.display();
 	}
 }
@@ -405,7 +473,7 @@ void  Menu::options_menu(RenderWindow& window)
 {
 	Menu menu4;
 	menu4.Hand_intilization();
-	menu4.font.loadFromFile("Assets/Fonts/Freedom-10eM.ttf");
+	menu4.font.loadFromFile("Assets/Fonts/HalloweenSlimePersonalUse-4B80D.otf");
 	menu4.choises = 3;
 
 	menu4.mainmenu[0].setFont(menu4.font);
@@ -453,20 +521,21 @@ void  Menu::options_menu(RenderWindow& window)
 					menu4.hand.setPosition(1140, 700);
 					pressed = true;
 				}
-				if (!Keyboard::isKeyPressed(Keyboard::Escape))
-				{
-					pressed = false;
-				}
-				if (event.key.code == Keyboard::Enter)
+				if (event.key.code == Keyboard::Enter || (event.key.code == Keyboard::Escape && !pressed))
 				{
 					sound.select_option_Sound();
 					if (menu4.selected == 0)  options_menu1(window);
 					if (menu4.selected == 2) {
 						pageNumber = 1000;
+						pressed = true;
 						return;
 					}
 				}
 			}
+		}
+		if (!Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			pressed = false;
 		}
 		menu_UI.FaceMotion(window);
 		window.clear();
@@ -500,6 +569,7 @@ void  Menu::instructions(RenderWindow& window)
 		{
 			sound.select_option_Sound();
 			pageNumber = 1000;
+			pressed = true;
 			return;
 		}
 		window.clear();
@@ -510,73 +580,104 @@ void  Menu::instructions(RenderWindow& window)
 void Menu::Pause(RenderWindow& window, Texture gametexture)
 {
 	Menu Pause1;
-	Pause1.choises = 3;
+	Pause1.choises = END + 2;
 	//Pause1.c = 3;
 	//if (!font.loadFromFile("arial.ttf")) {}
-	Pause1.font.loadFromFile("Assets/Fonts/Freedom-10eM.ttf");
+	if (END)
+		Pause1.mainmenu[0 + END].setFillColor(Color::Black);
+	else  
+		Pause1.mainmenu[0 + END].setFillColor(Color{ 255,204,0 });
+
+	Pause1.font.loadFromFile("Assets/Fonts/HalloweenSlimePersonalUse-4B80D.otf");
 	Pause1.mainmenu[0].setFont(Pause1.font);
 	Pause1.mainmenu[0].setFillColor(Color{ 255,204,0 });
-	Pause1.mainmenu[0].setString("continue");
-	Pause1.mainmenu[0].setCharacterSize(90);
-	Pause1.mainmenu[0].setPosition(800, 100);
+	Pause1.mainmenu[0].setString("resume");
+	Pause1.mainmenu[0].setCharacterSize(70);
+	Pause1.mainmenu[0].setPosition(670, 200);
 
-	Pause1.mainmenu[1].setFont(Pause1.font);
-	Pause1.mainmenu[1].setFillColor(sf::Color::Black);
-	Pause1.mainmenu[1].setString("resume");
-	Pause1.mainmenu[1].setCharacterSize(90);
-	Pause1.mainmenu[1].setPosition(800, 300);
+	Pause1.mainmenu[0+END].setFont(Pause1.font);
+	Pause1.mainmenu[0+END].setString("Play Again");
+	Pause1.mainmenu[0+END].setCharacterSize(70);
+	Pause1.mainmenu[0+END].setPosition(670, END?300:200);
 
-	Pause1.mainmenu[2].setFont(Pause1.font);
-	Pause1.mainmenu[2].setFillColor(sf::Color::Black);
-	Pause1.mainmenu[2].setString("exit");
-	Pause1.mainmenu[2].setCharacterSize(90);
-	Pause1.mainmenu[2].setPosition(800, 500);
+	Pause1.mainmenu[1+END].setFont(Pause1.font);
+	Pause1.mainmenu[1+END].setFillColor(sf::Color::Black);
+	Pause1.mainmenu[1+END].setString("Exit");
+	Pause1.mainmenu[1+END].setCharacterSize(70);
+	Pause1.mainmenu[1+END].setPosition(670, END?400:300);
 
 	RectangleShape photo2;
 	photo2.setSize(Vector2f(1920, 1080));
 	photo2.setFillColor(Color(0, 0, 0, 100));
+
 	Sprite game;
 	game.setTexture(gametexture);
 
+	Texture txx;
+	txx.loadFromFile("Assets/Textures/Pausemenu.png");
 
+	Sprite pausemenu;
+	pausemenu.setTexture(txx);
+	pausemenu.setPosition(600, 150);
+	pausemenu.setScale(1.5,1.8);
+	Pause1.Hand_intilization();
+	Pause1.hand.setPosition(560, 200);
+	Pause1.positionOfHand = 100;
+
+	//pressed = true;
 	while (window.isOpen()) {
-		sf::Event event;
+		Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
 			if (event.type == sf::Event::KeyReleased) {
 				if (event.key.code == sf::Keyboard::Up) {
 					sound.change_option_Sound();
-					Pause1.MoveUp(Pause1.selected, 3);
+					Pause1.MoveUp(Pause1.selected, Pause1.choises);
 					//break;
 				}
 				if (event.key.code == sf::Keyboard::Down) {
 					sound.change_option_Sound();
-					Pause1.MoveDown(Pause1.selected, 3);
+					Pause1.MoveDown(Pause1.selected, Pause1.choises);
 					//break;
 				}
-				if (event.key.code == sf::Keyboard::Enter) {
+				if (event.key.code == Keyboard::Escape && !pressed && Pause1.selected != 1 + END)
+				{
+					sound.change_option_Sound();
+					Pause1.mainmenu[Pause1.selected].setFillColor(Color::Black);
+					Pause1.selected = 1 + END;
+					Pause1.mainmenu[1 + END].setFillColor(Color{ 255,204,0 });
+					Pause1.hand.setPosition(560, END ? 400 : 300);
+					pressed = true;
+				}
+				if (event.key.code == sf::Keyboard::Enter || (event.key.code == Keyboard::Escape && !pressed)) {
 					sound.select_option_Sound();
-					if (Pause1.selected == 0) {
+					if (Pause1.selected == -1 + END) {
 						return;
 					}
-					if (Pause1.selected == 1) {
+					if (Pause1.selected == 0+END) {
 
 					}
-					if (Pause1.selected == 2) {
+					if (Pause1.selected == 1+END) {
 						window.setView(window.getDefaultView());
 						exit = 1;
 						return;
 					}
+				}
+				if (!Keyboard::isKeyPressed(Keyboard::Escape))
+				{
+					pressed = false;
 				}
 			}
 		}
 		window.clear();
 		window.draw(game);
 		window.draw(photo2);
-		for (int i = 0; i < 3; i++) {
+		window.draw(pausemenu);
+		for (int i = 0; i < Pause1.choises; i++) {
 			window.draw(Pause1.mainmenu[i]);
 		}
+		window.draw(Pause1.hand);
 		window.display();
 	}
 }

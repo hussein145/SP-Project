fbb00c8190dff7f55e664ccdd67bfb708e1f50bf
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-05-04 01:26:54 +0300

    Update Sounds.h

--- GameFiles/Headers/Sounds.h
+++ GameFiles/Headers/Sounds.h
@@ -3,11 +3,16 @@ struct Sounds
 {
 	int k = 0;
 	Music bgmusic;
-	SoundBuffer buf1, buf2;
-	Sound so, so2, so3;
+	SoundBuffer buf1, buf2, buf3, buf4, buf5, buf6;
+	Sound so, so2, so3, so4, so5, so6, so7;
 	void LoadMusic();
 	void music(int n);
 	void change_option_Sound();
 	void select_option_Sound();
+	void falling_sound();
+	void clock_ring_sound();
+	void hurry_up_sound();
+	void gonna_fall();
+
 };
 

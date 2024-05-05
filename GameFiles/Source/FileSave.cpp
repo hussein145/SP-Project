e745549a83262df139ac8c392cb59ce364645232
habibahatem173 <165413703+habibahatem173@users.noreply.github.com>
2024-05-05 17:52:38 +0300

    Update FileSave.cpp

--- GameFiles/Source/FileSave.cpp
+++ GameFiles/Source/FileSave.cpp
@@ -10,7 +10,84 @@
 #include "FileSave.h"
 
 extern Event event;
+extern Sounds sound;
+extern Menu menu;
 bool press = 0;
+void  FileSave::highscore(RenderWindow& window)
+{
+	Texture profiltex;
+	profiltex.loadFromFile("Assets/Textures/Capture.png");
+	RectangleShape profilesp;
+	profilesp.setTexture(&profiltex);
+	profilesp.setPosition(100, 0);
+	profilesp.setSize(Vector2f(1700, 1000));
+	Font hisfont;
+	hisfont.loadFromFile("Assets/Fonts/BrownieStencil-8O8MJ.ttf");
+	Text text[6], text2[6], text3[6], text4[6];
+	for (int i = 0; i < 5; i++)
+	{
+		text[i].setFont(hisfont);
+		text[i].setCharacterSize(30);
+		text[i].setFillColor(Color::Black);
+		text2[i].setFont(hisfont);
+		text2[i].setCharacterSize(30);
+		text2[i].setFillColor(Color::Black);
+		text3[i].setFont(hisfont);
+		text3[i].setCharacterSize(30);
+		text3[i].setFillColor(Color::Black);
+		text4[i].setFont(hisfont);
+		text4[i].setCharacterSize(30);
+		text4[i].setFillColor(Color::Black);
+
+	}
+	filetopair();
+	for (size_t i = 0; i < 5; i++)
+	{
+		string scoreStr[5];
+		scoreStr[i] = to_string(list[i].first);
+		text[i].setString(scoreStr[i]);
+		text[i].setPosition(500, 200 + ((i + 1) * 35));
+		text2[i].setString(list[i].second);
+		text2[i].setPosition(250, 200 + ((i + 1) * 35));
+		string floorStr[5];
+		floorStr[i] = to_string(list[i].first / 10);
+		text3[i].setString(floorStr[i]);
+		text3[i].setPosition(500, 550 + ((i + 1) * 35));
+		text4[i].setString(list[i].second);
+		text4[i].setPosition(250, 550 + ((i + 1) * 35));
+
+	}
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == event.Closed)
+				window.close();
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			sound.select_option_Sound();
+			menu.pageNumber = 1000;
+			return;
+		}
+
+		window.clear();
+		window.draw(profilesp);
+		for (int i = 0; i < 5; i++)
+		{
+			window.draw(text[i]);
+			window.draw(text2[i]);
+			window.draw(text3[i]);
+			window.draw(text4[i]);
+		}
+		window.display();
+	}
+
+
+
+}
 void FileSave::TypeYourName()
 {
 	if (event.type == Event::KeyPressed) {
@@ -25,6 +102,7 @@ void FileSave::TypeYourName()
 		}
 		else if (event.key.code == Keyboard::BackSpace)
 		{
+			if(playername.size())
 			playername.pop_back();
 		}
 		else if (event.key.code == Keyboard::Space)
@@ -63,7 +141,7 @@ void FileSave::EnterName() {
 
 }
 void FileSave::filetopair() {
-	fstream file("high score.txt", ios::in);
+	fstream file("Text.txt", ios::in);
 	if (file.is_open())
 	{
 		// Check if file is empty
@@ -77,7 +155,7 @@ void FileSave::filetopair() {
 		else {
 			// File is not empty; read data
 			for (int i = 0; i < 5; i++) {
-				file >> list[i].first >> list[i].second;
+				file >> list[i].second >> list[i].first;
 			}
 		}
 		file.close();
@@ -86,15 +164,18 @@ void FileSave::filetopair() {
 
 void FileSave::pairtofile()
 {
-	ofstream file("high score.txt", ios::out);
-	if (file.is_open()) {
-		for (int i = 0; i < 5; i++) {
-			file << list[i].first << "    " << list[i].second << "\n";
+	ofstream file("Text.txt", ios::out);
+	if (file.is_open()) 
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			file << list[i].second << "    " << list[i].first << "\n";
 		}
 		file.close();
 	}
-	else {
-		std::cerr << "Error opening file for writing." << std::endl;
+	else
+	{
+		cout << "Error opening file for writing." << endl;
 	}
 }
 void FileSave::intopair(int score)

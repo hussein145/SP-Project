c4c68fcda2a5434316a669193820465409941d8c
Ahmed Essam <71143516+ahmedessamsh@users.noreply.github.com>
2024-05-05 17:49:37 +0300

    Update PowerUps.h

--- GameFiles/Headers/PowerUps.h
+++ GameFiles/Headers/PowerUps.h
@@ -10,7 +10,7 @@ struct PowerUps
 	Sprite Drops[5]; // 0 walls inwards(heart)
 	float velocity_x = 20;
 	bool skip = 0;
-	float stopsmall = 0, stopbig = 0;
+	float stopsmall = 0;
 	float mapspeed = 1, addmapspeed = 0;
 	int rando;
 
@@ -24,4 +24,3 @@ struct PowerUps
 	void checkdrop(bool& start, bool& StartReturning);
 	void resetPowerups();
 };
-

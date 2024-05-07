bda97514bf8e089d7efb3ce16d7ce815fdd378a9
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-05-07 21:58:40 +0300

    Update FileSave.h

--- GameFiles2(Don't Use for now)/Headers/FileSave.h
+++ GameFiles2(Don't Use for now)/Headers/FileSave.h
@@ -4,12 +4,17 @@
 #include <string> // Include for std::string
 struct Data {
 	string Name;
-	int floor ;
-	int max_compo ;
+	int floor;
+	int max_compo;
 };
-
+struct user_data {
+	string user_name;
+	int  st_lvl2_score = 0, st_lvl3_score = 0, st_lvl4_score = 0, highest_stair, Index;
+	float  view_speed2, view_speed3, view_speed4;
+};
 struct FileSave
 {
+
 	pair <int, Data> list[6];
 	Texture enternameTX;
 	Sprite enternameSP;
@@ -25,7 +30,7 @@ struct FileSave
 
 	bool infile = 0;
 	bool boolenter = 0;
-
+	int index;
 	void filetopair();
 	void EnterName();
 	void TypeYourName();
@@ -33,6 +38,10 @@ struct FileSave
 	void intopair(int score);
 	void  highscore(RenderWindow& window);
 	void highscore_gameover(int Score, int floor, int combo);
-
+	void out_file();
+	void in_file();
+	void into_arr(string username, int stair, int score_lvl2, int score_lvl3, int score_lvl4,
+		float vx_lvl2, float vx_lvl3, float vx_lvl4);
+	void search(string username);
 };
 

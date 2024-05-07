4df99e016604f455416e6cfa5497f204a321ba60
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-05-07 21:59:00 +0300

    Update Messages.h

--- GameFiles2(Don't Use for now)/Headers/Messages.h
+++ GameFiles2(Don't Use for now)/Headers/Messages.h
@@ -5,10 +5,11 @@ struct Messages
 	//messages
 	Texture messages_tex[2];
 	Sprite message[2];
-	int appear = 0;
+	float timer1 = 10, timer2 = 10;
 	int Bounus = 0;
 	bool show = 0;
 	int cnt = 0;
+	bool cont1 = 0, cont2 = 0;
 
 	void messages();
 	void update_messages();

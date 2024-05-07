91365e17cfb84fd48adb23e2daa625da48bb5071
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-05-07 21:57:29 +0300

    Update Messages.cpp

--- GameFiles2(Don't Use for now)/Sources/Messages.cpp
+++ GameFiles2(Don't Use for now)/Sources/Messages.cpp
@@ -10,35 +10,30 @@ void Messages::messages()
 	message[0].setTexture(messages_tex[0]);
 	message[1].setTexture(messages_tex[1]);
 
-	message[0].setPosition(540, 900);
-	message[1].setPosition(540, 900);
+	message[0].setPosition(700, 700);
+	message[1].setPosition(700, 700);
 
 
-	message[0].setScale(3, 3);
+	message[0].setScale(3.5, 3.5);
 	message[1].setScale(3, 3);
 
 	Bounus = 0;
-	appear = 0;
+	timer1 = timer2 =  10;
 	show = 0;
 }
 void Messages::update_messages()
 {
 	if (player1.compo_cnt > 1 && cnt != player1.compo_cnt) { //2
-		Bounus++;
+		Bounus++; //1 2
 		cnt = player1.compo_cnt;
 
 	}
-	else if(player1.compo_cnt == 0 && Bounus == 0) {
-		
-		appear = 0;
-	}
 	if (player1.compo_cnt == 0 && Bounus > 0)
 	{
-		show = 1;
-		//if(Bounus )
-		
-		//meesage good
-
+		if (Bounus == 2)
+			timer1 = 0;
+		else if (Bounus >= 3)
+			timer2 = 0;
 	}
 
 

902990b0dac65d22ac5999172b2f45bdd16ef9a9
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-05-09 04:11:01 +0300

    Update Messages.cpp

--- GameFiles2(Use now)/Sources/Messages.cpp
+++ GameFiles2(Use now)/Sources/Messages.cpp
@@ -2,7 +2,7 @@
 #include "Messages.h"
 
 extern Players player1;
-void Messages::messages()
+void Messages::messages(int shift)
 {
 	messages_tex[0].loadFromFile("Assets//Textures//Good.png");
 	messages_tex[1].loadFromFile("Assets//Textures//Sweet!.png");
@@ -10,8 +10,8 @@ void Messages::messages()
 	message[0].setTexture(messages_tex[0]);
 	message[1].setTexture(messages_tex[1]);
 
-	message[0].setPosition(700, 700);
-	message[1].setPosition(700, 700);
+	message[0].setPosition(700 + shift, 700);
+	message[1].setPosition(700 + shift, 700);
 
 
 	message[0].setScale(3.5, 3.5);
@@ -20,17 +20,16 @@ void Messages::messages()
 	Bounus = show = cnt = bounus_points = 0;
 	timer = 10;
 }
-void Messages::update_messages()
+void Messages::update_messages(int compo_cnt)
 {
-	if (player1.compo_cnt > 1 && cnt != player1.compo_cnt) { //2
+	if (compo_cnt > 1 && cnt != compo_cnt) { //2
 		Bounus++; //1 2
-		cnt = player1.compo_cnt;
-		
+		cnt = compo_cnt;
+
 	}
-	if (player1.compo_cnt == 0 && Bounus > 1)
+	if (compo_cnt == 0 && Bounus > 1)
 	{
-		//cout << Bounus << endl;
-		bounus_points += cnt*cnt;
+		bounus_points += cnt * cnt;
 		cnt = 0;
 		if (Bounus == 2) {
 			appear = 1;
@@ -38,14 +37,14 @@ void Messages::update_messages()
 			Bounus = 0;
 			cnt = 0;
 		}
-		else if (Bounus >= 3){
+		else if (Bounus >= 3) {
 			appear = 2;
 			timer = 0;
 			Bounus = 0;
 			cnt = 0;
 		}
 	}
-	else if(player1.compo_cnt == 0)
+	else if (compo_cnt == 0)
 		Bounus = 0;
 
 

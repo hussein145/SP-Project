f12f0025a920bc4b3cdd794bc765943304f6a1bd
Ahmed Essam <71143516+ahmedessamsh@users.noreply.github.com>
2024-05-09 04:26:09 +0300

    Add files via upload

--- GameFiles2(Use now)/Headers/FileSave.h
+++ GameFiles2(Use now)/Headers/FileSave.h
@@ -6,6 +6,8 @@ struct Data {
 	string Name;
 	int floor;
 	int max_compo;
+	int min;
+	int sec;
 };
 struct user_data {
 	string user_name;
@@ -27,20 +29,21 @@ struct FileSave
 	Texture win, lose;
 	Sprite winner, losser;
 	Sprite gameoversp;
-	Text scoreText1, scoreText2, scoreText3;
+	Text scoreText1, scoreText2, scoreText3, scoreText4;
 	Font font;
 
 	int shift = 0;
 	bool infile = 0;
 	bool boolenter = 0;
 	int index;
+	int sec, min;
 	void filetopair();
 	void EnterName();
 	void TypeYourName();
 	void pairtofile();
 	void intopair(int score);
 	void  highscore(RenderWindow& window);
-	void highscore_gameover(int Score, int floor, int combo, int shift);
+	void highscore_gameover(int Score, int floor, int combo, int shift, int min, int sec);
 	void out_file();
 	void in_file();
 	void into_arr(string username, int stair, int score_lvl2, int score_lvl3, int score_lvl4,

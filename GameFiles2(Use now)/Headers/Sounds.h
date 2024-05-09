84a1f55a62936c55a13ca136a24728bff0ecbde8
ابانوب نصيف برزى مشرقى <153004639+BeboNasif@users.noreply.github.com>
2024-05-09 06:24:38 +0300

    Update Sounds.h

--- GameFiles2(Use now)/Headers/Sounds.h
+++ GameFiles2(Use now)/Headers/Sounds.h
@@ -2,18 +2,21 @@
 #include "include.h"
 struct Sounds
 {
-	int k = 0;
-	Music bgmusic;
-	sf::SoundBuffer buf1, buf2, buf3, buf4, buf5, buf6;
-	Sound so, so2, so3, so4, so5, so6, so7;
-	void LoadMusic();
-	void music(int n);
-	void change_option_Sound();
-	void select_option_Sound();
-	void falling_sound();
-	void clock_ring_sound();
-	void hurry_up_sound();
-	void gonna_fall();
+    int k = 0;
+    Music bgmusic;
+    SoundBuffer buf1, buf2, buf3, buf4;
+    Sound so, so2, so4, so5, so6, so7, so8, so9, so10, so11, so12, so13;
+    void LoadMusic(int n);
+    void music(int n);
+    void change_option_Sound();
+    void select_option_Sound();
+    void falling_sound();
+    void clock_ring_sound();
+    void hurry_up_sound();
+    void gonna_fall();
+    void jump();
+    void gameover();
+    void cheer();
+    void rotation_sound();
 
 };
-
